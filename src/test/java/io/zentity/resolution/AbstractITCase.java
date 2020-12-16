a388c1ccd6acbf23b8fb742f9799af0fc68bf5c7
austin ce <austin.cawley@gmail.com>
2020-12-16 10:38:43 -0500

    test: allow customizing rest client socket timeout

--- src/test/java/io/zentity/resolution/AbstractITCase.java
+++ src/test/java/io/zentity/resolution/AbstractITCase.java
@@ -5,6 +5,7 @@ import com.github.terma.javaniotcpproxy.StaticTcpProxyConfig;
 import com.github.terma.javaniotcpproxy.TcpProxy;
 import io.zentity.common.Json;
 import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
@@ -53,21 +54,30 @@ public abstract class AbstractITCase {
         .map(Duration::ofMillis)
         .orElseGet(() -> Duration.ofSeconds(5));
 
+    /**
+     * The max amount of socket inactivity in ms for the {@link RestClient ES RestClient}. Helpful to increase when
+     * stepping through breakpoints during plugin debuggin'.
+     */
+    private static final Duration CLIENT_SOCKET_TIMEOUT = Optional.ofNullable(System.getenv("CLIENT_SOCKET_TIMEOUT"))
+        .map(Long::valueOf)
+        .map(Duration::ofMillis)
+        .orElseGet(() -> DEBUGGER_ENABLED ? Duration.ofMinutes(5) : Duration.ofSeconds(30));
+
     protected static RestClient client;
 
     private static TcpProxy debuggerProxy;
 
     @ClassRule
-    public static final PluggableElasticsearchContainer ES_CONTAINER = getEsContainer();
+    public static final PluggableElasticsearchContainer ES_CONTAINER = buildEsContainer();
 
     private static Integer getRandomPort() {
         Random rand = new Random();
         return rand.ints(20_000, 60_000)
             .findFirst()
-            .getAsInt();
+            .orElseThrow(() -> new IllegalStateException("No random port generated"));
     }
 
-    public static PluggableElasticsearchContainer getEsContainer() {
+    private static PluggableElasticsearchContainer buildEsContainer() {
         PluggableElasticsearchContainer container = (PluggableElasticsearchContainer) new PluggableElasticsearchContainer(DEFAULT_IMAGE)
             .withPluginDir(Paths.get(PLUGIN_DIR))
             .withLogConsumer(new Slf4jLogConsumer(LOG));
@@ -78,6 +88,19 @@ public abstract class AbstractITCase {
         }
 
         return container;
+    }
+
+    private static RestClient buildRestClient() {
+        return RestClient
+            .builder(HttpHost.create(ES_CONTAINER.getHttpHostAddress()))
+            .setHttpClientConfigCallback(builder -> builder.setDefaultRequestConfig(
+                RequestConfig.custom()
+                    .setSocketTimeout(
+                        Long.valueOf(CLIENT_SOCKET_TIMEOUT.toMillis()).intValue()
+                    )
+                    .build()
+            ))
+            .build();
     }
 
     @BeforeClass
@@ -102,16 +125,16 @@ public abstract class AbstractITCase {
 
     @BeforeClass
     public static void startRestClient() throws IOException {
-        client = RestClient.builder(HttpHost.create(ES_CONTAINER.getHttpHostAddress())).build();
+        client = buildRestClient();
         try {
             Response response = client.performRequest(new Request("GET", "/"));
             JsonNode json = Json.MAPPER.readTree(response.getEntity().getContent());
             assertEquals("You Know, for Search", json.get("tagline").textValue());
         } catch (IOException e) {
+            stopRestClient();
             // If we have an exception here, let's ignore the test
             assumeFalse("Integration tests are skipped", e.getMessage().contains("Connection refused"));
             fail("Something wrong is happening. REST Client seemed to raise an exception: " + e.getMessage());
-            stopRestClient();
         }
     }
 
