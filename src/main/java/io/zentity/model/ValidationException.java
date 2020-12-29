3c972cca83f128495a67174af476e8685c657125
austin ce <austin.cawley@gmail.com>
2020-12-29 11:00:20 -0500

    refactor: make validationexception unchecked

--- src/main/java/io/zentity/model/ValidationException.java
+++ src/main/java/io/zentity/model/ValidationException.java
@@ -1,7 +1,6 @@
 package io.zentity.model;
 
-// TODO: does this need to be a checked exception?
-public class ValidationException extends Exception {
+public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
     }
